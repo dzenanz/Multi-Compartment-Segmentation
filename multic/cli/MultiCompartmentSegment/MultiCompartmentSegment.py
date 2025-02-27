90885529e8eb14aa087137d1481f43251e751002
Haitham Magdy <haitham.magdy@dxc.com>
2025-02-27 16:09:54 -0500

    remove prints

--- multic/cli/MultiCompartmentSegment/MultiCompartmentSegment.py
+++ multic/cli/MultiCompartmentSegment/MultiCompartmentSegment.py
@@ -1,46 +1,45 @@
+import torch
+from segmentationschool.segmentation_school import run_it
 import os
 import sys
 from ctk_cli import CLIArgumentParser
 import girder_client
 sys.path.append('..')
-from segmentationschool.segmentation_school import run_it
-import torch
 
 DEFAULT_VALS = {
-        'girderApiUrl':' ',
-        'girderToken':' ',
-        'input_file':' ',
-        'option':'predict',
-        'modelfile':None,
-        'white_percent':0.01,
-        'chop_thumbnail_resolution':16,
-        'overlap_percentHR':0,
-        'boxSize':2048,
-        'downsampleRateHR':1,
-        'Mag20X':False,
-        'roi_thresh':0.01,
-        'min_size':[30,30,30,30,30,30],
-        'bordercrop':300,
-        'show_interstitium':True
-    }
+    'girderApiUrl': ' ',
+    'girderToken': ' ',
+    'input_file': ' ',
+    'option': 'predict',
+    'modelfile': None,
+    'white_percent': 0.01,
+    'chop_thumbnail_resolution': 16,
+    'overlap_percentHR': 0,
+    'boxSize': 2048,
+    'downsampleRateHR': 1,
+    'Mag20X': False,
+    'roi_thresh': 0.01,
+    'min_size': [30, 30, 30, 30, 30, 30],
+    'bordercrop': 300,
+    'show_interstitium': True
+}
 
 
 def main(args):
     for name, value in os.environ.items():
         print(f"{name}={value}")
-    
+
     # Check if CUDA GPUs is available
     print(f"CUDAENV = {os.getenv('CUDA_VISIBLE_DEVICES')}")
     print(f"Allocated {torch.cuda.device_count()} cuda devices")
-    print(f"CUDA device name: {torch.cuda.get_device_name()}")
-    print(f"GPU is available: {torch.cuda.is_available()}")
 
     # If GPU available, or use cpu and set as args.gpu
-    if torch.cuda.is_available():
+    if torch.cuda.device_count() > 0:
+        # use the first GPU available
         args.gpu = 0
     else:
         args.gpu = -1
-        
+
     gc = girder_client.GirderClient(apiUrl=args.girderApiUrl)
     gc.setToken(args.girderToken)
 
@@ -58,11 +57,11 @@ def main(args):
         print('Using /mnt/girder_worker as mounted path')
         mounted_path = '{}/{}'.format('/mnt/girder_worker', os.listdir('/mnt/girder_worker')[0])
     else:
-        print('Using /tmp/ as mounted path') 
+        print('Using /tmp/ as mounted path')
         mounted_path = os.getenv('TMPDIR')
 
     # mounted_path = '{}/{}'.format('/mnt/girder_worker', os.listdir('/mnt/girder_worker')[0])
-    file_path = '{}/{}'.format(mounted_path,file_name)
+    file_path = '{}/{}'.format(mounted_path, file_name)
     gc.downloadFile(file_id, file_path)
 
     print(f'This is slide path: {file_path}')
@@ -76,18 +75,18 @@ def main(args):
 
     for d in DEFAULT_VALS:
         if d not in list(vars(args).keys()):
-            setattr(args,d,DEFAULT_VALS[d])
+            setattr(args, d, DEFAULT_VALS[d])
 
-    setattr(args,'item_id', item_id)
-    setattr(args,'file', file_path)
-    setattr(args,'gc', gc)
-
+    setattr(args, 'item_id', item_id)
+    setattr(args, 'file', file_path)
+    setattr(args, 'gc', gc)
 
     print(vars(args))
     for d in vars(args):
         print(f'argument: {d}, value: {getattr(args,d)}')
 
     run_it(args)
+
 
 if __name__ == "__main__":
     os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
