a5867075b16dbf148841c864314d313889446981
Dženan Zukić <dzenan.zukic@kitware.com>
2025-06-13 16:39:52 -0400

    Update SLURM script for FrozenHE+PAS mixed data training

--- multic/segmentationschool/run.sh
+++ multic/segmentationschool/run.sh
@@ -5,10 +5,10 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=96gb
 #SBATCH --partition=community
-#SBATCH --gpus=rtx6000:2
-#SBATCH --time=72:00:00
+#SBATCH --gpus=rtx6000:1
+#SBATCH --time=168:00:00
 #SBATCH --output=job%A.log
-#SBATCH --job-name="BANFF-AID-Training1"
+#SBATCH --job-name="PASfHE"
 echo "SLURM_JOBID="$SLURM_JOBID
 echo "SLURM_JOB_NODELIST="$SLURM_JOB_NODELIST
 echo "SLURM_NNODES="$SLURM_NNODES
@@ -20,6 +20,6 @@ which python
 
 echo "Launch job"
 CUDA_LAUNCH_BLOCKING=1
-python3 segmentation_school.py --option train --base_dir /home/local/KHQ/dzenan.zukic/Histo/ --init_modelfile /home/local/KHQ/dzenan.zukic/Histo/model_0214999.pth --training_data_dir /data/Public/banff-aid/TrainBig/ --train_steps 200000 --eval_period 500 --num_workers 2
+python3 segmentation_school.py --option train --base_dir /home/local/KHQ/dzenan.zukic/Histo/ --init_modelfile /home/local/KHQ/dzenan.zukic/Histo/model_0214999.pth --training_data_dir /data/Public/banff-aid/TrainMix/ --train_steps 200000 --eval_period 500 --num_workers 0
 
 echo "SLURM script Done!"
