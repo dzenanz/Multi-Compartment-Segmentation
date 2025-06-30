4ce1b1817019849573d82b2828523e0d09ee622e
Dženan Zukić <dzenan.zukic@kitware.com>
2025-06-30 11:28:33 -0400

    Switch time limit from 7 days to 28

--- multic/segmentationschool/run.sh
+++ multic/segmentationschool/run.sh
@@ -4,9 +4,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=96gb
-#SBATCH --partition=community
+#SBATCH --partition=emergency
 #SBATCH --gpus=rtx6000:1
-#SBATCH --time=168:00:00
+#SBATCH --time=28-23:00:00
 #SBATCH --output=job%A.log
 #SBATCH --job-name="PASfHE"
 echo "SLURM_JOBID="$SLURM_JOBID
@@ -20,6 +20,6 @@ which python
 
 echo "Launch job"
 CUDA_LAUNCH_BLOCKING=1
-python3 segmentation_school.py --option train --base_dir /home/local/KHQ/dzenan.zukic/Histo/ --init_modelfile /home/local/KHQ/dzenan.zukic/Histo/model_0214999.pth --training_data_dir /data/Public/banff-aid/TrainMix/ --train_steps 200000 --eval_period 500 --num_workers 0
+python3 segmentation_school.py --option train --base_dir /home/local/KHQ/dzenan.zukic/Histo/ --init_modelfile /home/local/KHQ/dzenan.zukic/Histo/model_0214999.pth --training_data_dir /data/Public/banff-aid/TrainMix/ --train_steps 200000 --eval_period 20000 --num_workers 0
 
 echo "SLURM script Done!"
